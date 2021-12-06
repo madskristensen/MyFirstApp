cc2f66577dfdf6369579a6e4cdfad947563b4688
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-12-06 12:03:36 -0800

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello world");
+
+
+
+
+
+
+
+
+
+var text = "Hello World";
+Console.WriteLine(text);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
